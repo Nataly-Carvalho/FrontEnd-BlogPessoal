57cb37b7e48cdaf1f3c9f88cc500ea1d18aaaed1
Nataly <natalycarvalho.dev@gmail.com>
2022-09-05 15:55:28 -0300

    update

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
   styleUrls: ['./entrar.component.css']
-})
-export class EntrarComponent implements OnInit {
+ })
+ export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
   }
 
-}
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+      this.userLogin =resp
+      this.router.navigate(['/inicio'])
+  
+    }, erro=>{
+      if(erro.status == 500){
+        alert('Usuario ou senha estão incorretos!')
+      }
+    })
+
+  }
+
+} 
---
+++ src/app/model/User.ts
@@ -0,0 +1,13 @@
+import { Postagem } from "./Postagem";
+
+export class User{
+
+    public id: number;
+    public nome: string;
+    public usuario: string;
+    public senha: string;
+    public foto: string;
+    public tipo: string;
+    public postagem: Postagem[]
+
+}
