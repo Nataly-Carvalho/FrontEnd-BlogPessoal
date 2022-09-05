a9051af1990b358cca9fcd6b38ba6f81f3d967ba
Nataly <natalycarvalho.dev@gmail.com>
2022-09-05 16:42:17 -0300

    consumo

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { AuthService } from '../service/auth.service';
 
@@ -23,11 +24,21 @@ import { AuthService } from '../service/auth.service';
 
   entrar(){
     this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
-      this.userLogin =resp
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome= this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+      
+      console.log(environment.token)
+      console.log(environment.nome)
+      console.log(environment.foto)
+      console.log(environment.id)
       this.router.navigate(['/inicio'])
   
     }, erro=>{
-      if(erro.status == 500){
+      if(erro.status == 401){
         alert('Usuario ou senha estão incorretos!')
       }
     })
