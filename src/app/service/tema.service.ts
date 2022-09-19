7480d0bce9c3704b09fdeea6389ef5a709cdc37b
Nataly Carvalho <natalycarvalho.dev@gmail.com>
2022-09-19 01:57:20 -0300

    Update tema.service.ts

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -18,23 +18,23 @@ token={
 }
 
     getAllTema(): Observable<Tema[]>{
-     return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+     return this.http.get<Tema[]>('https://natalyblogpessoal.herokuapp.com/tema', this.token)
     }
 
     getByIdTema(id: number): Observable<Tema>{
-      return this.http.get<Tema>(`http://localhost:8080/tema/${id}`, this.token)
+      return this.http.get<Tema>(`https://natalyblogpessoal.herokuapp.com/tema/${id}`, this.token)
     }
 
     postTema(tema: Tema): Observable<Tema>{
-      return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+      return this.http.post<Tema>('https://natalyblogpessoal.herokuapp.com/tema', tema, this.token)
     }
     
     putTema(tema: Tema):Observable<Tema>{
-      return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+      return this.http.post<Tema>('https://natalyblogpessoal.herokuapp.com/tema', tema, this.token)
     }
 
     deleteTema(id: number){
-      return this.http.delete(`http://localhost:8080/tema/${id}`, this.token)
+      return this.http.delete(`https://natalyblogpessoal.herokuapp.com/tema/${id}`, this.token)
     }
   }
 
