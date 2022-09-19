60d5224a8e7c28c5a47bde48b58b9f541ef00e08
Nataly Carvalho <natalycarvalho.dev@gmail.com>
2022-09-19 01:36:43 -0300

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,18 +15,18 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://natalyblogpessoal.herokuapp.com/usuarios/logar', userLogin)
 
   }
   cadastrar(user:User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://natalyblogpessoal.herokuapp.com/cadastrar', user)
   }
   atualizar(user:User): Observable<User>{
-    return this.http.put<User>('http://localhost:8080/usuarios/atualizar', user)
+    return this.http.put<User>('https://natalyblogpessoal.herokuapp.com/atualizar', user)
   }
 
   getByIdUser(id:number): Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<User>(`https://natalyblogpessoal.herokuapp.com/usuarios/${id}`)
   }
   logado(){
     let ok: boolean = false
