97d6d9e1de98bb3ab06fd2b66b70bbfdfdfacc1b
Nataly Carvalho <natalycarvalho.dev@gmail.com>
2022-09-19 01:57:56 -0300

    Update postagem.service.ts

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -16,20 +16,20 @@ export class PostagemService {
   }
 
   getAllPostagens(): Observable<Postagem[]>{
-    return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
+    return this.http.get<Postagem[]>('https://natalyblogpessoal.herokuapp.com/postagens', this.token)
   }
   getByIdPostagem(id: number): Observable<Postagem>{
-    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.get<Postagem>(`https://natalyblogpessoal.herokuapp.com/postagens/${id}`, this.token)
   }
 
   postPostagem(postagem: Postagem): Observable<Postagem>{
-    return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.post<Postagem>('https://natalyblogpessoal.herokuapp.com/postagens', postagem, this.token)
   }
 
   putPostagem(postagem: Postagem):Observable<Postagem>{
-    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+    return this.http.put<Postagem>('https://natalyblogpessoal.herokuapp.com/postagens', postagem, this.token)
   }
   deletePostagem(id: number){
-    return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
+    return this.http.delete(`https://natalyblogpessoal.herokuapp.com/postagens/${id}`, this.token)
   }
 }
